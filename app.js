e4a22728fae06ae4961767d3274f887b3f37abd3
anonymousdomain <domainanonymous1@gmail.com>
2021-11-17 12:03:03 +0300

    first routes tetsed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { Router } from 'express';
 import bodyParser from 'body-Parser'
+import router from './routes/users.js';
 //init express
 const app=express();
 
 const port=process.env.PORT||5000;
 
 app.use(bodyParser.json())
-
+app.use('/',router)
 app.listen(port,()=>console.log(`server is running on port:http://localhost:${port}`))
