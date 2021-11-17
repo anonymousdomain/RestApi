328024135d4999bdc883f3f415318088e0790a12
anonymousdomain <domainanonymous1@gmail.com>
2021-11-17 12:06:03 +0300

    basic routes

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,18 @@ import express from "express";
 const router=express.Router();
 
 
-router.get('/',(req,res)=>res.send("hello"))
+router.get('/users',(req,res)=>res.send("hello"))
+
+router.get('/users/:id',getUsers)
+
+router.post('/users',createUser)
 
 
+router.put('users/:id',updateUserInfo)
 
+router.patch('/users/:id',patchUserInfo)
+
+router.delete('/users/:id',deleteUser)
 
 
 
